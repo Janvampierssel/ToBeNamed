e7c3602c9b52ddfa8e9172e5f53e4c4683b97831
pcc137071 <59567371+pcc137071@users.noreply.github.com>
2020-01-31 18:27:04 +0100

    First upload of all the game files, only fps controller working

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed;
+    public float shiftSpeed;
+    public float gravity;
+    public float jumpHeight;
+    public float airMultiplier;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+    float airSpeed;
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (!isGrounded)
+        {
+            airSpeed = 1 * airMultiplier;
+        }
+        else
+        {
+            airSpeed = 1;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+
+        if (!Input.GetButton("Shift"))
+        {
+            controller.Move(move * speed * airSpeed * Time.deltaTime);
+        }
+
+        else
+        {
+            controller.Move(move * shiftSpeed * airSpeed * Time.deltaTime);
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
